7e129e3ab94989004e7b7c975ee1d665792ce531
macrespo42 <macrespo@student.42.fr>
2025-09-23 14:24:14 +0200

    refacto: move http handlers in api dir

---
+++ src/app/api/readiness.ts
@@ -0,0 +1,9 @@
+import type { Request, Response } from "express";
+
+export async function handlerReadiness(
+  _: Request,
+  res: Response,
+): Promise<void> {
+  res.set("Content-Type", "text/plain; charset=utf-8");
+  res.send("OK");
+}
--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,10 @@
 import express from "express";
+import { handlerReadiness } from "./app/api/readiness.js";
 
 const app = express();
 const PORT = 8080;
 
 app.use("/app", express.static("./src/app"));
-
-async function handlerReadiness(
-  _: express.Request,
-  res: express.Response,
-): Promise<void> {
-  res.set("Content-Type", "text/plain; charset=utf-8");
-  res.send("OK");
-}
 
 app.get("/healthz", handlerReadiness);
 
