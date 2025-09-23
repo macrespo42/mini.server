6ddb65e1414d43ec9888e4dd31a0b632bae75fd9
macrespo42 <macrespo@student.42.fr>
2025-09-23 14:32:07 +0200

    feat: add middlewares to check for non-ok response

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,12 @@
 import express from "express";
 import { handlerReadiness } from "./app/api/readiness.js";
+import { middlewareLogResponses } from "./middlewares/logResponses.js";
 
 const app = express();
 const PORT = 8080;
 
 app.use("/app", express.static("./src/app"));
+app.use(middlewareLogResponses);
 
 app.get("/healthz", handlerReadiness);
 
---
+++ src/middlewares/logResponses.ts
@@ -0,0 +1,16 @@
+import type { NextFunction, Request, Response } from "express";
+
+export function middlewareLogResponses(
+  req: Request,
+  res: Response,
+  next: NextFunction,
+) {
+  res.on("finish", () => {
+    if (res.statusCode !== 200) {
+      console.log(
+        `[NON-OK] ${req.method} ${req.url} - Status: ${res.statusCode}`,
+      );
+    }
+  });
+  next();
+}
