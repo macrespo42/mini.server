fcb8970abf029670ddcc7b3de241576339828d90
macrespo42 <macrespo@student.42.fr>
2025-09-23 14:14:43 +0200

    feat: setup healtz endpoint

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,20 @@
 import express from "express";
 
 const app = express();
+const PORT = 8080;
 
-app.listen(8080);
+app.use("/app", express.static("./src/app"));
+
+async function handlerReadiness(
+  _: express.Request,
+  res: express.Response,
+): Promise<void> {
+  res.set("Content-Type", "text/plain; charset=utf-8");
+  res.send("OK");
+}
+
+app.get("/healthz", handlerReadiness);
+
+app.listen(PORT, () => {
+  console.log(`Server is running at localhost:${PORT}`);
+});
