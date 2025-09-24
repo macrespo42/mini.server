442da3d3d51fd4bcce8c8d2ddfb52e23d6a1f2d4
macrespo42 <macrespo@student.42.fr>
2025-09-24 16:31:08 +0200

    feat: first queries

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,16 @@
+process.loadEnvFile();
+
+const DB_URL = process.env.DB_URL;
+if (!DB_URL) {
+  throw new Error("DB_URL is undefined");
+}
+
 type APIConfig = {
   fileServerHits: number;
+  dbURL: string;
 };
 
 export const config: APIConfig = {
   fileServerHits: 0,
+  dbURL: DB_URL,
 };
